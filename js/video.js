c10f53f9e9706efa9f324e34f64359f1b10f01dc
ezequielcutin <ezecutin@umich.edu>
2024-03-31 16:07:56 -0400

    javascript changes!

--- js/video.js
+++ js/video.js
@@ -2,10 +2,63 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video = document.getElementById("player1");
+	video.autoplay = false;
+	video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.getElementById("play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	console.log("Volume is: " + video.volume);
+});
 
+document.getElementById("pause").addEventListener("click", function(){
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.getElementById("slower").addEventListener("click",function(){
+	video.playbackRate *= 0.9;
+	console.log("The new playback speed is: " + video.playbackRate);
+});
+
+document.getElementById("faster").addEventListener("click",function(){
+	video.playbackRate /= 0.9;
+	console.log("The new playback speed is: " + video.playbackRate);
+});
+
+document.getElementById("skip").addEventListener("click",function(){
+	if(video.currentTime + 10 > video.duration){
+		video.currentTime = 0;
+		console.log("Skipped back to the start of the video");
+	}
+	else{
+		video.currentTime+=10;
+	}
+	console.log("Current location is: " + video.currentTime);
+});
+
+document.getElementById("mute").addEventListener("click",function(){
+	if(video.muted){
+		video.muted = false;
+		this.innerHTML = "Mute";
+	}
+	else{
+		video.muted = true;
+		this.innerHTML = "Unmute";
+	}
+});
+
+document.getElementById("slider").addEventListener("change",function(){
+	video.volume = this.value/100;
+	document.getElementById("volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.getElementById("vintage").addEventListener("click",function(){
+	video.classList.add("oldSchool");
+});
+
+document.getElementById("orig").addEventListener("click",function(){
+	video.classList.remove("oldSchool");
+});
